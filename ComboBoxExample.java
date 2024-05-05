cacff84ce100ddca76e76f82d9076080631ace4a
snega0606 <155870976+snega0606@users.noreply.github.com>
2024-05-05 16:44:03 +0530

    Update ComboBoxExample.java

--- ComboBoxExample.java
+++ ComboBoxExample.java
@@ -15,4 +15,15 @@ ComboBoxExample(){
 public static void main(String[] args) {    
     new ComboBoxExample();         
 }    
-}   
+}
+
+
+// this is a sample to pull the file from the git.
+ f=new JFrame("ComboBox Example");    
+    String program[]={"lab1","lab2","lab3","lab4","lab5","lab6"};        
+    JComboBox cb=new JComboBox(program);    
+    cb.setBounds(50, 50,90,20);    
+    f.add(cb);        
+    f.setLayout(null);    
+    f.setSize(400,500);    
+    f.setVisible(true);    
